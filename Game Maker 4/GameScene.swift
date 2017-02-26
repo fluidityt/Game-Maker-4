7a3426799aacaa4c099129389a9990a7c7ed66da
fluidityt <hackbook17@justins-macbook-pro.local>
2017-02-26 07:32:00 -0500

    autocommit

--- Game Maker 4/GameScene.swift
+++ Game Maker 4/GameScene.swift
@@ -9,84 +9,79 @@
 import SpriteKit
 import GameplayKit
 
+fileprivate let boxSize = CGSize(width: 25, height: 25)
+
+enum sys {
+  
+  static var currentNode: SKNode?
+  static var scene: SKScene = SKScene()
+  
+}
+
+class Prompt: SKSpriteNode {
+
+  init(title: String) {
+    super.init(texture: nil, color: .blue, size: boxSize)
+    isUserInteractionEnabled = true
+    name = ("PROMPT: " + title)
+  }
+  
+  override func mouseDown(with event: NSEvent) {
+    sys.currentNode = self
+  }
+  
+  required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented")  }
+}
+
+class Button: SKSpriteNode {
+  
+  init(color: SKColor, size: CGSize) {
+    super.init(texture: nil, color: color, size: size)
+    isUserInteractionEnabled = true
+  }
+  
+  required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented")  }
+}
+
+class AddButton: Button {
+  override func mouseDown(with event: NSEvent) {
+    if let curNode = sys.currentNode {
+      print(curNode.name as Any, "selected")
+    } else { print("no node selected") }
+  }
+}
+
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+
+  private func initialize() {
+    // Laundry list:
+    sys.scene = self
+  }
+  
+  override func didMove(to view: SKView) {
+    initialize()
+  }
+  
+  override func mouseDown(with event: NSEvent) {
+
+  }
+  
+  override func mouseDragged(with event: NSEvent) {
+
+  }
+  
+  override func mouseUp(with event: NSEvent) {
+
+  }
+  
+  override func keyDown(with event: NSEvent) {
+    switch event.keyCode {
+      default: print("keyDown: \(event.characters!) keyCode: \(event.keyCode)")
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func mouseDown(with event: NSEvent) {
-        self.touchDown(atPoint: event.location(in: self))
-    }
-    
-    override func mouseDragged(with event: NSEvent) {
-        self.touchMoved(toPoint: event.location(in: self))
-    }
-    
-    override func mouseUp(with event: NSEvent) {
-        self.touchUp(atPoint: event.location(in: self))
-    }
-    
-    override func keyDown(with event: NSEvent) {
-        switch event.keyCode {
-        case 0x31:
-            if let label = self.label {
-                label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-            }
-        default:
-            print("keyDown: \(event.characters!) keyCode: \(event.keyCode)")
-        }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+  }
+  
+  
+  override func update(_ currentTime: TimeInterval) {
+    // Called before each frame is rendered
+  }
 }
---
+++ Game Maker 4/ok.swift
@@ -0,0 +1,12 @@
+//
+//  ok.swift
+//  Game Maker 4
+//
+//  Created by justin fluidity on 2/26/17.
+//  Copyright © 2017 justin fluidity. All rights reserved.
+//
+
+import SpriteKit
+
+
+
